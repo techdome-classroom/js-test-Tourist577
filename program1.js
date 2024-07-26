3eaef342c3c9551b770d1fe5795b02bfdc0892be
backend-emesbee <backenddeveloper9@gmail.com>
2024-07-26 13:33:50 +0530

    submit

--- program1.js
+++ program1.js
@@ -1,7 +1,31 @@
+
+function func(grid,m,n){
+
+  if(m>=grid.length || m<0 || n<0 || n>=grid[0].length || grid[m][n]==='W') return ;
+
+  grid[m][n]='W';
+
+  func(grid,m+1,n);
+  func(grid,m-1,n);
+  func(grid,m,n+1);
+  func(grid,m,n-1);
+  
+}
+
 const getTotalIsles = function (grid) {
 
 
   // write your code here
+  let ans=0;
+
+  for(let i=0;i<grid.length;i++){
+    for(let j=0;j<grid[0].length;j++){
+      if(grid[i][j]=='L'){
+        ans++;
+        func(grid,i,j);
+      }
+    }
+  }
 
 };
 
--- program2.js
+++ program2.js
@@ -1,7 +1,33 @@
+
+function func(s,p,index1,index2){
+  
+  if(index1==s.length && index2==p.length) return true;
+  
+  let ans=false;
+
+  if(s[index1]===p[index2]){
+    ans=ans | func(s,p,index+1,index2+1);
+  }
+  else if(p[index2]==='?'){
+      ans=ans | func(s,p,index1+1,index2+1);
+    }
+  else if(p[index2]==='*'){
+
+      
+        ans=ans | func(s,p,index1+1,index2) | func(s,p,index1,index2+1);
+    }
+    else{
+        return false;
+    }
+  
+  return ans;
+}
+
 const decodeTheRing = function (s, p) {
 
     // write your code here
 
+    return func(s,p,0,0);
   };
   
   module.exports = decodeTheRing;
